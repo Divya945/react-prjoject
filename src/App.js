3fa5278e9588b8ed8250ed4bae40de474bbfc20b
Divya945 <divyadvs945@gmail.com>
2020-09-16 18:51:36 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -3,14 +3,39 @@ import "./style.css";
 import HeadName from './Component/Component'
 
 class App extends Component {
+  constructor(){
+    this.state = {
+      id: 1011,
+      name: "Alex",
+      job: "Developer",
+      salary: 2222
+    };
+  }
+
+
   render() { 
     return (
-    <div className="App">
-      <h1>Hello StackBlitz!</h1>
-        <p>Start editing to see some magic happen :)</p>  
-        <p>Start editing to see some magic happen :)</p>
-      <p>Start editing to see some magic happen :)</p>
+    <div className = "App">
+      <button>Form Button</button>
       <HeadName />
+      <HeadName name ="first" type="firsttype" />
+      <HeadName name ="second" type="secondtype" > category : Second</HeadName>
+      <HeadName name ="third" type="thirdtype" />
+      <table>
+      <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>JobTitle</th>
+        <th>Salary</th>
+      </tr>
+        <tr>
+          <td>{this.state.id}</td>
+          <td>{this.state.name}</td>
+          <td>{this.state.job}</td>
+          <td>{this.state.salary}</td>
+
+        </tr>
+      </table>
     </div>
     );
   }
--- src/Component/Component.js
+++ src/Component/Component.js
@@ -1,6 +1,11 @@
 import React from "react";
-const headName = () => {
-return <h3>This is the First page IN Project</h3>
+const headName = (props) => {
+return (
+  <div>
+    <p>This is {props.name} component.. And the type is {props.type} {props.children}</p>
+    <p></p>
+  </div>
+)
 };
 
 export default headName;
